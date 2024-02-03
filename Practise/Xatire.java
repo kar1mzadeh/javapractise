cbbf4409715a925404b93f53b7bed607ca3b5472
kar1mzadeh <147240457+kar1mzadeh@users.noreply.github.com>
2024-02-03 23:42:46 +0400

    some

--- Practise/Xatire.java
+++
@@ -1,7 +0,0 @@
-public class Xatire {
-    public static void main(String args[]) {
-        StringBuffer buffer = new StringBuffer("Rahman");
-        buffer.append(" ").append("Camal");
-        System.out.println(buffer);
-    }
-}
